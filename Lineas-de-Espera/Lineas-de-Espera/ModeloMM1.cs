d908030a27d36fc6350d5b10d095bb9eab7c0e9f
Drumarcco <marcoantonioam9@gmail.com>
2014-12-03 09:01:28 -0700

    Creacion de clase ModeloMM1
    Esta clase implementa los metodos abstractos de ModeloLineaDeEspera
    dando la logica necesaria para el MM1, aun quedan algunos metodos por
    implementar.

---
+++ Lineas-de-Espera/Lineas-de-Espera/ModeloMM1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lineas_de_Espera
+{
+    class ModeloMM1 : ModeloLineaDeEspera
+    {
+        public ModeloMM1(float tasaMediaTiempoLlegadaClientes, float tasaMediaTiempoServicio )
+        {
+            this.numeroServidores = 1;
+            this.tasaMediaTiempoLlegadaClientes = tasaMediaTiempoLlegadaClientes;
+            this.tasaMediaTiempoServicio = tasaMediaTiempoServicio;
+            setFactorUtilizacion();
+
+        }
+
+        public override void setNumeroEsperadoClientesSistema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override float calcularNumeroEsperadoClientesSistema(float factorUtilizacion)
+        {
+            float numeroEsperadoClientesSistema;
+            numeroEsperadoClientesSistema = factorUtilizacion / (1 - factorUtilizacion);
+            return numeroEsperadoClientesSistema;
+        }
+
+
+    }
+}
