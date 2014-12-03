5dc6196d4e0deab40091015ab5199e0dba94c1cc
Drumarcco <marcoantonioam9@gmail.com>
2014-12-03 08:58:02 -0700

    Creacion de clase abstracta ModeloLineaDeEspera
    Esta clase sirve como plantilla para la creacion de todos los modelos de
    Lineas de Espera, de uno o mas servidores.

---
+++ Lineas-de-Espera/Lineas-de-Espera/ModeloLineaDeEspera.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lineas_de_Espera
+{
+    abstract class ModeloLineaDeEspera
+    {
+        #region Member Variables
+        private float _tasaMediaTiempoLlegadaClientes;  // Lambda λ
+        private float _tasaMediaTiempoServicio;         // Mu μ
+        private float _numeroEsperadoClientesSistema;   // L
+        private float _numeroEsperadoClientesFila;      // Lq
+        private float _tiempoEsperaEstimadoSistema;     // W
+        private float _tiempoEsperaEstimadoFila;        // Wq
+        private float _numeroServidores;                // s
+        private float _factorUtilizacion;               // Rho
+        #endregion
+
+        #region Getters & Setters
+        public float tasaMediaTiempoLlegadaClientes 
+        { 
+            get { return _tasaMediaTiempoLlegadaClientes; }
+            set { _tasaMediaTiempoLlegadaClientes = value; }
+        }   
+        public float tasaMediaTiempoServicio 
+        {
+            get { return _tasaMediaTiempoServicio; }
+            set { _tasaMediaTiempoServicio = value; }
+        }
+        public float numeroEsperadoClientesSistema
+        {
+            get { return _numeroEsperadoClientesSistema; }
+            private set;
+        }
+
+        public float numeroEsperadoClientesFila
+        {
+            get { return _numeroEsperadoClientesFila; }
+            private set;
+        }
+
+        public float tiempoEsperaEstimadoSistema
+        {
+            get { return _tiempoEsperaEstimadoSistema; }
+            private set;
+        }
+
+        public float tiempoEsperaEstimadoFila
+        {
+            get { return _tiempoEsperaEstimadoFila; }
+            private set;
+        }
+
+        public float numeroServidores
+        {
+            get { return _numeroServidores; }
+            set { _numeroServidores = value; }
+        }
+        public float factorUtilizacion { get; private set; }
+        public void setFactorUtilizacion()
+        {
+            _factorUtilizacion = calcularFactorUtilizacion(_tasaMediaTiempoLlegadaClientes, tasaMediaTiempoServicio);
+        }
+
+        #region Abstract Setters
+        public abstract void setNumeroEsperadoClientesSistema();
+        public abstract void setNumeroEsperadoClientesFila();
+        public abstract void setTiempoEsperaEstimadoSistema();
+        public abstract void setTiempoEsperaEstimadoFila();
+        #endregion       
+        
+        #endregion
+
+        #region Global Methods
+        public float invertirValor(float valor)
+        {
+            return 1 / valor;
+        }
+
+        private float calcularFactorUtilizacion(float lambda, float mu)
+        {
+            return lambda / mu;
+        }
+        #endregion
+
+        #region Abstract Methods
+        abstract public float calcularNumeroEsperadoClientesSistema(float factorUtilizacion);
+        abstract public float calcularNumeroEsperadoClientesFila();
+        abstract public float calcularTiempoEsperaSistema();
+        abstract public float calcularTiempoEsperaFila();
+        #endregion
+
+    }
+}
