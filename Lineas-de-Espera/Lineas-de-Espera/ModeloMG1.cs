3bba2dc910b02e6d6279a7d68901b3d82e33a4b5
Vaztor29 <jufravazto@hotmail.com>
2014-12-04 18:03:05 -0600

    Se Agrego la CLase MG1 (Por Segunda Vez -.-)
    Unicamente Clase y Modificaciones de los metodos Abstractos
    correspondientes

---
+++ Lineas-de-Espera/Lineas-de-Espera/ModeloMG1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lineas_de_Espera
+{
+    class ModeloMG1
+    {
+    }
+}
