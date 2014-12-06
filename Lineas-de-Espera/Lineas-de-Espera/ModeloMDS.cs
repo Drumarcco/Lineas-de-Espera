8c7f6f72f3b12e808fdc7f8e2d8192ea5c52f261
Drumarcco <marcoantonioam9@gmail.com>
2014-12-06 12:21:09 -0700

    Creacion de MDS
    Estructura para construir valores de la grafica para el modelo MDS.

---
+++ Lineas-de-Espera/Lineas-de-Espera/ModeloMDS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lineas_de_Espera
+{
+    public class ModeloMDS : ModeloLineaDeEspera
+    {
+        List<ValoresMDS> valoresMDS = new List<ValoresMDS>()
+        {
+            new ValoresMDS{ rho = 0.1f, s = 1, l = 0.1f },
+            new ValoresMDS{ rho = 0.1f, s = 2, l = 0.17f },
+            new ValoresMDS{ rho = 0.1f, s = 3, l = 0.25f },
+        };
+
+    }
+}
---
+++ Lineas-de-Espera/Lineas-de-Espera/ValoresMDS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lineas_de_Espera
+{
+    public class ValoresMDS
+    {
+        public float rho;
+        public int s;
+        public float l;
+    }
+}
