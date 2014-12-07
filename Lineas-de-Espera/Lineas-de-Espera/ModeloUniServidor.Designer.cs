b2d0fb397538ad95fe6e11b5c85d0cb0dbb2f660
Drumarcco <marcoantonioam9@gmail.com>
2014-12-07 15:46:17 -0700

    Implementacion para todos los modelos

--- Lineas-de-Espera/Lineas-de-Espera/ModeloUniServidor.Designer.cs
+++ Lineas-de-Espera/Lineas-de-Espera/ModeloUniServidor.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txt_mu = new System.Windows.Forms.TextBox();
-            this.txt_lambda = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -44,24 +42,22 @@
             this.lbl_tiempoEsperaCola = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lbx_probabilidadNClientes = new System.Windows.Forms.ListBox();
+            this.lbl_tituloDesviacionEstandar = new System.Windows.Forms.Label();
+            this.lbl_desviacionEstandar = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl_numeroServidores = new System.Windows.Forms.Label();
+            this.lbl_lambda = new System.Windows.Forms.Label();
+            this.lbl_mu = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_Tiempo = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbl_wMayorT = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lbl_wqMayorT = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // txt_mu
-            // 
-            this.txt_mu.Location = new System.Drawing.Point(197, 45);
-            this.txt_mu.Name = "txt_mu";
-            this.txt_mu.Size = new System.Drawing.Size(100, 20);
-            this.txt_mu.TabIndex = 10;
-            this.txt_mu.TextChanged += new System.EventHandler(this.txt_mu_TextChanged);
-            // 
-            // txt_lambda
-            // 
-            this.txt_lambda.Location = new System.Drawing.Point(197, 15);
-            this.txt_lambda.Name = "txt_lambda";
-            this.txt_lambda.Size = new System.Drawing.Size(100, 20);
-            this.txt_lambda.TabIndex = 9;
-            this.txt_lambda.TextChanged += new System.EventHandler(this.txt_lambda_TextChanged);
             // 
             // label3
             // 
@@ -84,7 +80,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(78, 77);
+            this.label1.Location = new System.Drawing.Point(78, 108);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(114, 13);
             this.label1.TabIndex = 11;
@@ -93,7 +89,7 @@
             // lbl_factorUtilizacion
             // 
             this.lbl_factorUtilizacion.AutoSize = true;
-            this.lbl_factorUtilizacion.Location = new System.Drawing.Point(198, 77);
+            this.lbl_factorUtilizacion.Location = new System.Drawing.Point(198, 108);
             this.lbl_factorUtilizacion.Name = "lbl_factorUtilizacion";
             this.lbl_factorUtilizacion.Size = new System.Drawing.Size(10, 13);
             this.lbl_factorUtilizacion.TabIndex = 12;
@@ -102,7 +98,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 106);
+            this.label4.Location = new System.Drawing.Point(12, 137);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(210, 13);
             this.label4.TabIndex = 13;
@@ -111,7 +107,7 @@
             // lbl_numeroEsperadoClientesSistema
             // 
             this.lbl_numeroEsperadoClientesSistema.AutoSize = true;
-            this.lbl_numeroEsperadoClientesSistema.Location = new System.Drawing.Point(229, 106);
+            this.lbl_numeroEsperadoClientesSistema.Location = new System.Drawing.Point(229, 137);
             this.lbl_numeroEsperadoClientesSistema.Name = "lbl_numeroEsperadoClientesSistema";
             this.lbl_numeroEsperadoClientesSistema.Size = new System.Drawing.Size(10, 13);
             this.lbl_numeroEsperadoClientesSistema.TabIndex = 14;
@@ -120,7 +116,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 137);
+            this.label5.Location = new System.Drawing.Point(12, 168);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(201, 13);
             this.label5.TabIndex = 15;
@@ -129,7 +125,7 @@
             // lbl_numeroEsperadoClientesFila
             // 
             this.lbl_numeroEsperadoClientesFila.AutoSize = true;
-            this.lbl_numeroEsperadoClientesFila.Location = new System.Drawing.Point(219, 137);
+            this.lbl_numeroEsperadoClientesFila.Location = new System.Drawing.Point(219, 168);
             this.lbl_numeroEsperadoClientesFila.Name = "lbl_numeroEsperadoClientesFila";
             this.lbl_numeroEsperadoClientesFila.Size = new System.Drawing.Size(10, 13);
             this.lbl_numeroEsperadoClientesFila.TabIndex = 16;
@@ -138,7 +134,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 169);
+            this.label6.Location = new System.Drawing.Point(12, 200);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(173, 13);
             this.label6.TabIndex = 17;
@@ -147,7 +143,7 @@
             // lbl_tiempoEsperaSistema
             // 
             this.lbl_tiempoEsperaSistema.AutoSize = true;
-            this.lbl_tiempoEsperaSistema.Location = new System.Drawing.Point(191, 169);
+            this.lbl_tiempoEsperaSistema.Location = new System.Drawing.Point(191, 200);
             this.lbl_tiempoEsperaSistema.Name = "lbl_tiempoEsperaSistema";
             this.lbl_tiempoEsperaSistema.Size = new System.Drawing.Size(10, 13);
             this.lbl_tiempoEsperaSistema.TabIndex = 18;
@@ -156,7 +152,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 200);
+            this.label7.Location = new System.Drawing.Point(12, 231);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(153, 13);
             this.label7.TabIndex = 19;
@@ -165,7 +161,7 @@
             // lbl_tiempoEsperaCola
             // 
             this.lbl_tiempoEsperaCola.AutoSize = true;
-            this.lbl_tiempoEsperaCola.Location = new System.Drawing.Point(171, 200);
+            this.lbl_tiempoEsperaCola.Location = new System.Drawing.Point(171, 231);
             this.lbl_tiempoEsperaCola.Name = "lbl_tiempoEsperaCola";
             this.lbl_tiempoEsperaCola.Size = new System.Drawing.Size(10, 13);
             this.lbl_tiempoEsperaCola.TabIndex = 20;
@@ -189,11 +185,142 @@
             this.lbx_probabilidadNClientes.Size = new System.Drawing.Size(229, 173);
             this.lbx_probabilidadNClientes.TabIndex = 0;
             // 
+            // lbl_tituloDesviacionEstandar
+            // 
+            this.lbl_tituloDesviacionEstandar.AutoSize = true;
+            this.lbl_tituloDesviacionEstandar.Location = new System.Drawing.Point(313, 231);
+            this.lbl_tituloDesviacionEstandar.Name = "lbl_tituloDesviacionEstandar";
+            this.lbl_tituloDesviacionEstandar.Size = new System.Drawing.Size(108, 13);
+            this.lbl_tituloDesviacionEstandar.TabIndex = 22;
+            this.lbl_tituloDesviacionEstandar.Text = "Desviación Estándar:";
+            this.lbl_tituloDesviacionEstandar.Visible = false;
+            // 
+            // lbl_desviacionEstandar
+            // 
+            this.lbl_desviacionEstandar.AutoSize = true;
+            this.lbl_desviacionEstandar.Location = new System.Drawing.Point(427, 231);
+            this.lbl_desviacionEstandar.Name = "lbl_desviacionEstandar";
+            this.lbl_desviacionEstandar.Size = new System.Drawing.Size(10, 13);
+            this.lbl_desviacionEstandar.TabIndex = 23;
+            this.lbl_desviacionEstandar.Text = "-";
+            this.lbl_desviacionEstandar.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(68, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "s Numero de Servidores:";
+            // 
+            // lbl_numeroServidores
+            // 
+            this.lbl_numeroServidores.AutoSize = true;
+            this.lbl_numeroServidores.Location = new System.Drawing.Point(198, 78);
+            this.lbl_numeroServidores.Name = "lbl_numeroServidores";
+            this.lbl_numeroServidores.Size = new System.Drawing.Size(10, 13);
+            this.lbl_numeroServidores.TabIndex = 25;
+            this.lbl_numeroServidores.Text = "-";
+            // 
+            // lbl_lambda
+            // 
+            this.lbl_lambda.AutoSize = true;
+            this.lbl_lambda.Location = new System.Drawing.Point(198, 18);
+            this.lbl_lambda.Name = "lbl_lambda";
+            this.lbl_lambda.Size = new System.Drawing.Size(10, 13);
+            this.lbl_lambda.TabIndex = 26;
+            this.lbl_lambda.Text = "-";
+            // 
+            // lbl_mu
+            // 
+            this.lbl_mu.AutoSize = true;
+            this.lbl_mu.Location = new System.Drawing.Point(198, 48);
+            this.lbl_mu.Name = "lbl_mu";
+            this.lbl_mu.Size = new System.Drawing.Size(10, 13);
+            this.lbl_mu.TabIndex = 27;
+            this.lbl_mu.Text = "-";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl_wqMayorT);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.lbl_wMayorT);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.txt_Tiempo);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Location = new System.Drawing.Point(18, 274);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(533, 108);
+            this.groupBox2.TabIndex = 28;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Probabilidades de tiempo de espera excedente:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(8, 23);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "t Tiempo:";
+            // 
+            // txt_Tiempo
+            // 
+            this.txt_Tiempo.Location = new System.Drawing.Point(65, 20);
+            this.txt_Tiempo.Name = "txt_Tiempo";
+            this.txt_Tiempo.Size = new System.Drawing.Size(100, 20);
+            this.txt_Tiempo.TabIndex = 1;
+            this.txt_Tiempo.TextChanged += new System.EventHandler(this.txt_Tiempo_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(11, 53);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(43, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "P(W>t):";
+            // 
+            // lbl_wMayorT
+            // 
+            this.lbl_wMayorT.AutoSize = true;
+            this.lbl_wMayorT.Location = new System.Drawing.Point(66, 53);
+            this.lbl_wMayorT.Name = "lbl_wMayorT";
+            this.lbl_wMayorT.Size = new System.Drawing.Size(10, 13);
+            this.lbl_wMayorT.TabIndex = 3;
+            this.lbl_wMayorT.Text = "-";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(11, 76);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(49, 13);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "P(Wq>t):";
+            // 
+            // lbl_wqMayorT
+            // 
+            this.lbl_wqMayorT.AutoSize = true;
+            this.lbl_wqMayorT.Location = new System.Drawing.Point(66, 76);
+            this.lbl_wqMayorT.Name = "lbl_wqMayorT";
+            this.lbl_wqMayorT.Size = new System.Drawing.Size(10, 13);
+            this.lbl_wqMayorT.TabIndex = 5;
+            this.lbl_wqMayorT.Text = "-";
+            // 
             // ModeloUniServidor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 335);
+            this.ClientSize = new System.Drawing.Size(580, 421);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.lbl_mu);
+            this.Controls.Add(this.lbl_lambda);
+            this.Controls.Add(this.lbl_numeroServidores);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbl_desviacionEstandar);
+            this.Controls.Add(this.lbl_tituloDesviacionEstandar);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lbl_tiempoEsperaCola);
             this.Controls.Add(this.label7);
@@ -205,13 +332,13 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.lbl_factorUtilizacion);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txt_mu);
-            this.Controls.Add(this.txt_lambda);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Name = "ModeloUniServidor";
             this.Text = "ModeloUniServidor";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,8 +346,6 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txt_mu;
-        private System.Windows.Forms.TextBox txt_lambda;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
@@ -235,5 +360,18 @@
         private System.Windows.Forms.Label lbl_tiempoEsperaCola;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ListBox lbx_probabilidadNClientes;
+        private System.Windows.Forms.Label lbl_tituloDesviacionEstandar;
+        private System.Windows.Forms.Label lbl_desviacionEstandar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl_numeroServidores;
+        private System.Windows.Forms.Label lbl_lambda;
+        private System.Windows.Forms.Label lbl_mu;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_Tiempo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbl_wqMayorT;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lbl_wMayorT;
+        private System.Windows.Forms.Label label10;
     }
 }
--- Lineas-de-Espera/Lineas-de-Espera/ModeloUniServidor.cs
+++ Lineas-de-Espera/Lineas-de-Espera/ModeloUniServidor.cs
@@ -12,16 +12,23 @@ namespace Lineas_de_Espera
 {
     public partial class ModeloUniServidor : Form
     {
+        #region Member Variables
         ModeloMM1 _modeloMM1;
         ModeloMD1 _modeloMD1;
         ModeloMG1 _modeloMG1;
+        ModeloMMS _modeloMMS;
+        ModeloMDS _modeloMDS;
+        #endregion
+
+        #region Constructors
         public ModeloUniServidor(ModeloMM1 modeloMM1)
         {
             InitializeComponent();
             this.Text = "Modelo M/M/1";
             _modeloMM1 = modeloMM1;
-            txt_lambda.Text = modeloMM1.tasaMediaTiempoLlegadaClientes.ToString();
-            txt_mu.Text = modeloMM1.tasaMediaTiempoServicio.ToString();
+            lbl_lambda.Text = modeloMM1.tasaMediaTiempoLlegadaClientes.ToString();
+            lbl_mu.Text = modeloMM1.tasaMediaTiempoServicio.ToString();
+            lbl_numeroServidores.Text = modeloMM1.numeroServidores.ToString();
             lbl_factorUtilizacion.Text = modeloMM1.factorUtilizacion.ToString();
             lbl_numeroEsperadoClientesSistema.Text = modeloMM1.numeroEsperadoClientesSistema.ToString();
             lbl_numeroEsperadoClientesFila.Text = modeloMM1.numeroEsperadoClientesFila.ToString();
@@ -36,8 +43,9 @@ namespace Lineas_de_Espera
             InitializeComponent();
             this.Text = "Modelo M/G/1";
             _modeloMG1 = modeloMG1;
-            txt_lambda.Text = modeloMG1.tasaMediaTiempoLlegadaClientes.ToString() ;
-            txt_mu.Text = modeloMG1.tasaMediaTiempoServicio.ToString();
+            lbl_lambda.Text = modeloMG1.tasaMediaTiempoLlegadaClientes.ToString() ;
+            lbl_mu.Text = modeloMG1.tasaMediaTiempoServicio.ToString();
+            lbl_numeroServidores.Text = modeloMG1.numeroServidores.ToString();
             lbl_factorUtilizacion.Text = modeloMG1.factorUtilizacion.ToString();
             lbl_numeroEsperadoClientesSistema.Text = modeloMG1.numeroEsperadoClientesSistema.ToString();
             lbl_numeroEsperadoClientesFila.Text = modeloMG1.numeroEsperadoClientesFila.ToString();
@@ -45,14 +53,18 @@ namespace Lineas_de_Espera
             lbl_tiempoEsperaCola.Text = modeloMG1.tiempoEsperaEstimadoFila.ToString();
             List<string> listaProbabilidades = modeloMG1.generarListaProbabilidadesClientes(modeloMG1.factorUtilizacion);
             llenarListaProbabilidades(listaProbabilidades);
+            lbl_tituloDesviacionEstandar.Visible = true;
+            lbl_desviacionEstandar.Visible = true;
+            lbl_desviacionEstandar.Text = modeloMG1.desviacionEstandar.ToString();
         }
 
         public ModeloUniServidor(ModeloMD1 modeloMD1){
             InitializeComponent();
             this.Text = "Modelo M/D/1";
             _modeloMD1 = modeloMD1;
-            txt_lambda.Text = modeloMD1.tasaMediaTiempoLlegadaClientes.ToString();
-            txt_mu.Text = modeloMD1.tasaMediaTiempoServicio.ToString();
+            lbl_lambda.Text = modeloMD1.tasaMediaTiempoLlegadaClientes.ToString();
+            lbl_mu.Text = modeloMD1.tasaMediaTiempoServicio.ToString();
+            lbl_numeroServidores.Text = modeloMD1.numeroServidores.ToString();
             lbl_factorUtilizacion.Text = modeloMD1.factorUtilizacion.ToString();
             lbl_numeroEsperadoClientesSistema.Text = modeloMD1.numeroEsperadoClientesSistema.ToString();
             lbl_numeroEsperadoClientesFila.Text = modeloMD1.numeroEsperadoClientesFila.ToString();
@@ -62,28 +74,48 @@ namespace Lineas_de_Espera
             llenarListaProbabilidades(listaProbabilidades);
         }
 
-        private void llenarListaProbabilidades(List<string> listaProbabilidades)
+        public ModeloUniServidor(ModeloMMS modeloMMS)
         {
-            foreach (string probabilidad in listaProbabilidades){
-                lbx_probabilidadNClientes.Items.Add(probabilidad);
-            }
+            InitializeComponent();
+            this.Text = "Modelo M/M/S";
+            _modeloMMS = modeloMMS;
+            lbl_lambda.Text = modeloMMS.tasaMediaTiempoLlegadaClientes.ToString();
+            lbl_mu.Text = modeloMMS.tasaMediaTiempoServicio.ToString();
+            lbl_numeroServidores.Text = modeloMMS.numeroServidores.ToString();
+            lbl_factorUtilizacion.Text = modeloMMS.factorUtilizacion.ToString();
+            lbl_numeroEsperadoClientesSistema.Text = modeloMMS.numeroEsperadoClientesSistema.ToString();
+            lbl_numeroEsperadoClientesFila.Text = modeloMMS.numeroEsperadoClientesFila.ToString();
+            lbl_tiempoEsperaSistema.Text = modeloMMS.numeroEsperadoClientesSistema.ToString();
+            lbl_tiempoEsperaCola.Text = modeloMMS.numeroEsperadoClientesFila.ToString();
+            List<string> listaProbabilidades = modeloMMS.generarListaProbabilidadesClientes(
+                modeloMMS.tasaMediaTiempoLlegadaClientes,
+                modeloMMS.tasaMediaTiempoServicio,
+                modeloMMS.numeroServidores);
+            llenarListaProbabilidades(listaProbabilidades);
         }
 
-        private void calcularProbabilidadesTiempoExcedente()
+        public ModeloUniServidor(ModeloMDS modeloMDS)
         {
-            if (this.Text.Contains("M/M/1"))
-            {
-                throw new NotImplementedException();
-            }
-            else if (this.Text.Contains("M/G/1"))
-            {
-                throw new NotImplementedException();
-            }
-            else if (this.Text.Contains("M/D/1"))
-            {
-                throw new NotImplementedException();
-            }
+            InitializeComponent();
+            this.Text = "Modelo M/D/S";
+            _modeloMDS = modeloMDS;
+            lbl_lambda.Text = modeloMDS.tasaMediaTiempoLlegadaClientes.ToString();
+            lbl_mu.Text = modeloMDS.tasaMediaTiempoServicio.ToString();
+            lbl_numeroServidores.Text = modeloMDS.numeroServidores.ToString();
+            lbl_factorUtilizacion.Text = modeloMDS.factorUtilizacion.ToString();
+            lbl_numeroEsperadoClientesSistema.Text = modeloMDS.numeroEsperadoClientesSistema.ToString();
+            lbl_numeroEsperadoClientesFila.Text = modeloMDS.numeroEsperadoClientesFila.ToString();
+            lbl_tiempoEsperaSistema.Text = modeloMDS.tiempoEsperaEstimadoSistema.ToString();
+            lbl_tiempoEsperaCola.Text = modeloMDS.tiempoEsperaEstimadoFila.ToString();
+            List<string> listaProbabilidades = modeloMDS.generarListaProbabilidadesClientes(
+                modeloMDS.tasaMediaTiempoLlegadaClientes,
+                modeloMDS.tasaMediaTiempoServicio,
+                modeloMDS.numeroServidores);
+            llenarListaProbabilidades(listaProbabilidades);
         }
+        #endregion
+
+        #region Event Handlers
         private void txt_mu_TextChanged(object sender, EventArgs e)
         {
 
@@ -92,6 +124,103 @@ namespace Lineas_de_Espera
         private void txt_lambda_TextChanged(object sender, EventArgs e)
         {
 
+        }
+        #endregion
+
+        #region Other Methods
+        private void llenarListaProbabilidades(List<string> listaProbabilidades)
+        {
+            foreach (string probabilidad in listaProbabilidades)
+            {
+                lbx_probabilidadNClientes.Items.Add(probabilidad);
+            }
+        }
+        private void calcularProbabilidadesTiempoExcedente(float tiempo)
+        {
+            if (_modeloMM1 != null)
+            {
+                calcularProbabilidadesTiempoExcedenteMM1(_modeloMM1, tiempo);
+            }
+            else if (_modeloMG1 != null)
+            {
+                calcularProbabilidadesTiempoExcedenteMG1(_modeloMG1, tiempo);
+            }
+            else if (_modeloMD1 != null)
+            {
+                calcularProbabilidadesTiempoExcedenteMD1(_modeloMD1, tiempo);
+            }
+            else if (_modeloMMS != null)
+            {
+
+            }
+            else if (_modeloMDS != null)
+            {
+
+            }
+        }
+
+        private void calcularProbabilidadesTiempoExcedenteMM1(ModeloMM1 modelo, float tiempo)
+        {
+            float PWt;
+            float PQwt;
+
+            PWt = modelo.calcularProbabilidadTiempoEsperaSistemaExcedente(modelo.tasaMediaTiempoServicio, modelo.factorUtilizacion, tiempo);
+            PQwt = modelo.calcularProbabilidadTiempoEsperaFilaExcedente(modelo.factorUtilizacion, modelo.tasaMediaTiempoServicio, tiempo);
+            lbl_wMayorT.Text = PWt.ToString();
+            lbl_wqMayorT.Text = PQwt.ToString();
+        }
+
+        private void calcularProbabilidadesTiempoExcedenteMG1(ModeloMG1 modelo, float tiempo)
+        {
+            float PWt;
+            float PQwt;
+
+            PWt = modelo.calcularProbabilidadTiempoEsperaSistemaExcedente(modelo.tasaMediaTiempoServicio, modelo.factorUtilizacion, tiempo);
+            PQwt = modelo.calcularProbabilidadTiempoEsperaFilaExcedente(modelo.factorUtilizacion, modelo.tasaMediaTiempoServicio, tiempo);
+            lbl_wMayorT.Text = PWt.ToString();
+            lbl_wqMayorT.Text = PWt.ToString();
+        }
+
+        private void calcularProbabilidadesTiempoExcedenteMD1(ModeloMD1 modelo, float tiempo)
+        {
+            float PWt;
+            float PQwt;
+
+            PWt = modelo.calcularProbabilidadTiempoEsperaSistemaExcedente(modelo.tasaMediaTiempoServicio, modelo.factorUtilizacion, tiempo);
+            PQwt = modelo.calcularProbabilidadTiempoEsperaFilaExcedente(modelo.tasaMediaTiempoServicio, modelo.factorUtilizacion, tiempo);
+            lbl_wMayorT.Text = PWt.ToString();
+            lbl_wqMayorT.Text = PQwt.ToString();
+        }
+
+        private void calcularProbabilidadesTiempoExcedenteMMS(ModeloMDS modelo, float tiempo)
+        {
+            float PWt;
+            float PQwt;
+
+            
+        }
+        #endregion
+
+        private void txt_Tiempo_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_Tiempo.Text != "")
+            {
+                try
+                {
+                    float tiempo = float.Parse(txt_Tiempo.Text);
+                    if (tiempo < 0) throw new ArgumentException("El tiempo no puede ser negativo.");
+                    calcularProbabilidadesTiempoExcedente(tiempo);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("El formato del texto introducido es incorrecto.");
+                    txt_Tiempo.Clear();
+                }
+                catch (ArgumentException argException)
+                {
+                    MessageBox.Show(argException.Message);
+                }
+            }
         }
     }
 }
